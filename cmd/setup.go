414f13bcabd1a1307fe2b2a8b4345f7265f4d572
Thales Gelinger <thales.gelinger@em.com>
2024-10-23 14:10:33 -0300

    feat: add setup

--- cmd/setup.go
+++ cmd/setup.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"os/user"
+	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -21,29 +23,59 @@ var setupCmd = &cobra.Command{
 }
 
 func setup(cmd *cobra.Command, args []string) {
-	fmt.Println("setup called")
-	createFolderIfNotExist("~/.config/celeste")
-	repoUrl := getRepoUrl()
-	fmt.Println("repo: %s", repoUrl)
-	// clone repo on .config/celeste using bare mode
-}
 
-func createFolderIfNotExist(path string) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		// Create the directory
-		err := os.Mkdir(path, 0755) // 0755 is the permission
-		if err != nil {
-			fmt.Println("Error creating directory:", err)
-			return
-		}
-		fmt.Println("Directory created:", path)
-	} else {
-		fmt.Println("Directory already exists:", path)
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println("Error getting current user:", err)
+		return
+	}
+
+	userPath := filepath.Join(usr.HomeDir, ".local", "share", "celeste")
+	fmt.Println("setup called")
+	err = createFolderIfNotExist(userPath)
+	if err != nil {
+		log.Fatal("Error creating celeste folder", err.Error())
+	}
+	repo, err := getRepoInfo()
+	if err != nil {
+		log.Fatal("Error getting repo info", err.Error())
+	}
+	err = cloneBareRepo(repo.Url, repo.Name, userPath)
+	if err != nil {
+		log.Fatal("Error cloning repo", err.Error())
 	}
 }
 
-func getRepoUrl() string {
-	// git remote -v | awk '/fetch/ {print $2}'
+func cloneBareRepo(repoUrl, repoName, targetDir string) error {
+	cmd := exec.Command("git", "clone", "--bare", repoUrl, fmt.Sprintf("%s/%s", targetDir, repoName))
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error cloning repository:", err.Error())
+		fmt.Println("Output:", string(output))
+		return err
+	}
+	fmt.Println("Repository cloned successfully into:", targetDir)
+	return nil
+}
+
+func createFolderIfNotExist(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			return err
+		}
+		fmt.Println("Directory created:", path)
+	}
+	return nil
+}
+
+type Repo struct {
+	Url  string
+	Name string
+}
+
+func getRepoInfo() (Repo, error) {
 	gitCmd := exec.Command("git", "remote", "-v")
 
 	pipe, err := gitCmd.StdoutPipe()
@@ -53,7 +85,7 @@ func getRepoUrl() string {
 
 	if err := gitCmd.Start(); err != nil {
 		log.Fatal("Error starting git command:", err)
-		return ""
+		return Repo{}, err
 	}
 
 	awkCmd := exec.Command("awk", "/fetch/ {print $2}")
@@ -62,20 +94,28 @@ func getRepoUrl() string {
 	awkOutput, err := awkCmd.Output()
 	if err != nil {
 		log.Fatal("Error running awk command:", err)
-		return ""
+		return Repo{}, err
 	}
 
 	if err := gitCmd.Wait(); err != nil {
 		log.Fatal("Error waiting for git command:", err)
-		return ""
+		return Repo{}, err
 	}
 
-	if err := awkCmd.Wait(); err != nil {
-		log.Fatal("Error waiting for awk command:", err)
-		return ""
+	cwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println("Error getting current directory:", err)
+		return Repo{}, err
 	}
 
-	return strings.TrimSpace(string(awkOutput))
+	// Extract the last element of the path
+	dirName := filepath.Base(cwd)
+
+	return Repo{
+		Url:  strings.TrimSpace(string(awkOutput)),
+		Name: dirName,
+	}, nil
+
 }
 
 func init() {
